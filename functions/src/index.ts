48c55d0508b553b1c1d6a50a9a78e5264f496b8c
Akram Rasikh <akram.rasikh@gmail.com>
2025-05-13 22:55:29 +0100

    addSnippetRoute added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,12 @@ import * as functions from 'firebase-functions';
 import { onLoadDataRoute } from './on-load-data';
 import { translateTextRoute } from './translate-text';
 import { textToSpeechRoute } from './text-to-speech';
+import { addSnippetRoute } from './routes/snippets/add-snippet';
 
 exports.translateText = functions.https.onRequest(translateTextRoute);
 
 exports.textToSpeech = functions.https.onRequest(textToSpeechRoute);
 
 exports.getOnLoadData = functions.https.onRequest(onLoadDataRoute);
+
+exports.addSnippet = functions.https.onRequest(addSnippetRoute);
