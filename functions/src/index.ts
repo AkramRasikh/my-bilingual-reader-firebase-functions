b9ead3e62dc790e2a94052de91fae08c4eae1630
Akram Rasikh <akram.rasikh@gmail.com>
2025-05-14 15:55:01 +0100

    delete snippet route working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import { onLoadDataRoute } from './on-load-data';
 import { translateTextRoute } from './translate-text';
 import { textToSpeechRoute } from './text-to-speech';
 import { addSnippetRoute } from './routes/snippets/add-snippet';
+import { deleteSnippetRoute } from './routes/snippets/delete-snippet';
 
 exports.translateText = functions.https.onRequest(translateTextRoute);
 
@@ -11,3 +12,5 @@ exports.textToSpeech = functions.https.onRequest(textToSpeechRoute);
 exports.getOnLoadData = functions.https.onRequest(onLoadDataRoute);
 
 exports.addSnippet = functions.https.onRequest(addSnippetRoute);
+
+exports.deleteSnippet = functions.https.onRequest(deleteSnippetRoute);
