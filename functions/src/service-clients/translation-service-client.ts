264c65ca3f1544dbab947ffde22a146cb4b03c1a
Akram Rasikh <akram.rasikh@gmail.com>
2025-05-15 20:38:46 +0100

    string type

--- functions/src/service-clients/translation-service-client.ts
+++ functions/src/service-clients/translation-service-client.ts
@@ -2,5 +2,5 @@ import { TranslationServiceClient } from '@google-cloud/translate';
 import config from '../config';
 
 export const translationClient = new TranslationServiceClient({
-  credentials: JSON.parse(config.googleTranslateAccount),
+  credentials: JSON.parse(config.googleTranslateAccount as string),
 });
