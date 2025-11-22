242eaa4808f6be725f6cf359492d218ea93585aa
Akram Rasikh <akram.rasikh@gmail.com>
2025-11-22 15:53:01 +0300

    deleted snippet related code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,6 @@ import * as functions from 'firebase-functions';
 import { onLoadDataRoute } from './routes/on-load-data';
 import { translateTextRoute } from './routes/translate-text';
 import { textToSpeechRoute } from './routes/text-to-speech';
-import { addSnippetRoute } from './routes/snippets/add-snippet';
-import { deleteSnippetRoute } from './routes/snippets/delete-snippet';
 import { addWordRoute } from './routes/words/add-word';
 import { updateWordRoute } from './routes/words/update-word';
 import { deleteWordRoute } from './routes/words/delete-word';
@@ -24,10 +22,6 @@ exports.translateText = functions.https.onRequest(translateTextRoute);
 exports.textToSpeech = functions.https.onRequest(textToSpeechRoute);
 
 exports.getOnLoadData = functions.https.onRequest(onLoadDataRoute);
-
-exports.addSnippet = functions.https.onRequest(addSnippetRoute);
-
-exports.deleteSnippet = functions.https.onRequest(deleteSnippetRoute);
 
 exports.addWord = functions.https.onRequest(addWordRoute);
 
